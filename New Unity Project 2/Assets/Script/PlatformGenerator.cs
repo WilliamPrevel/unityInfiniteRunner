81853188c8c6098e434e9b8d65e12681305f7731
William <wprevel9@gmail.com>
2018-10-17 16:24:31 -0400

    UpdateFiles

---
+++ New Unity Project 2/Assets/Script/PlatformGenerator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformGenerator : MonoBehaviour {
+
+    //Public Variables
+    public GameObject platforms;
+    public Transform generation;
+    public float distBetween;
+    //Private Variables
+    private float platformWidth;
+
+    // Use this for initialization
+    void Start () {
+        platformWidth = platforms.GetComponent<BoxCollider2D>().size.x;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(transform.position.x < generation.position.x)
+        {
+            transform.position = new Vector3(transform.position.x + platformWidth + distBetween, transform.position.y, transform.position.z);
+
+            Instantiate(platforms, transform.position, transform.rotation);
+        }
+	}
+}
