aef69d0f864a7fb14c8f0aae1a9c492f8c67578b
William <wprevel9@gmail.com>
2018-10-18 13:14:13 -0400

    Infinite Runner
    Edited and added Object Pools

---
+++ New Unity Project 2/Assets/Script/CupeGenerator.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CupeGenerator : MonoBehaviour{
+
+    //Public Variables
+    public GameObject Cube;
+    public Transform generation;
+    public float distBetween;
+    //Private Variables
+    private float cubeWidth;
+    private float distance = Random.Range(1.0f, 10.0f);
+
+    public ObjPool theObjectPool;
+
+    // Use this for initialization
+    void Start()
+    {
+        cubeWidth = Cube.GetComponent<BoxCollider2D>().size.x;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x < generation.position.x)
+        {
+            transform.position = new Vector3(transform.position.x + cubeWidth + distBetween, transform.position.y, transform.position.z);
+
+
+            //Instantiate(platforms, transform.position, transform.rotation)
+            GameObject newCube = theObjectPool.GetPooledObject();
+            distance = Random.Range(1.0f, 10.0f);
+            newCube.transform.position = transform.position;
+            newCube.transform.rotation = transform.rotation;
+            newCube.SetActive(true);
+
+        }
+    }
+}
--- New Unity Project 2/Assets/Script/PlatformGenerator.cs
+++ New Unity Project 2/Assets/Script/PlatformGenerator.cs
@@ -11,19 +11,29 @@ public class PlatformGenerator : MonoBehaviour {
     //Private Variables
     private float platformWidth;
 
+    public ObjPool theObjectPool;
+
     // Use this for initialization
-    void Start () {
+    void Start ()
+    {
         platformWidth = platforms.GetComponent<BoxCollider2D>().size.x;
 
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 		if(transform.position.x < generation.position.x)
         {
             transform.position = new Vector3(transform.position.x + platformWidth + distBetween, transform.position.y, transform.position.z);
 
-            Instantiate(platforms, transform.position, transform.rotation);
+            //Instantiate(platforms, transform.position, transform.rotation);
+            GameObject newPlatform = theObjectPool.GetPooledObject();
+
+            newPlatform.transform.position = transform.position;
+            newPlatform.transform.rotation = transform.rotation;
+            newPlatform.SetActive(true);
+
         }
 	}
 }
